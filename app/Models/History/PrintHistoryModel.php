379326e8f97d2a127bfbc4569602f55c057f0169
gajn01 <88372302+gajn01@users.noreply.github.com>
2024-04-04 14:33:08 +0800

    add model,migrate and controller

---
+++ app/Models/History/PrintHistoryModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\History;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PrintHistoryModel extends Model
+{
+    use HasFactory;
+    protected $table = 'print_history';
+    protected $fillable = [
+        'batch_id',
+        'produce_items',
+        'is_reprint',
+        'reason',
+        'attachment',
+    ];
+}
