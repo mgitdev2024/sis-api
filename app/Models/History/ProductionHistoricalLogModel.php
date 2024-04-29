c2d0de89764ba1592aca5591ffa07b127effdeaf
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2024-04-29 08:55:59 +0800

    set initial print, metal line batch

---
+++ app/Models/History/ProductionHistoricalLogModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\History;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductionHistoricalLogModel extends Model
+{
+    use HasFactory;
+}
