28e1422397387aed0fc276fffb70130fbd3e0f00
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2024-05-31 14:06:43 +0800

    edit in acess

--- database/migrations/2024_05_27_020716_create_access_submodule_permissions.php
+++ database/migrations/2024_05_27_020716_create_access_submodule_permissions.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('submodule_permissions', function (Blueprint $table) {
+        Schema::create('access_submodule_permissions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('module_permission_id');
             $table->string('name');
@@ -33,6 +33,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('submodule_permissions');
+        Schema::dropIfExists('access_submodule_permissions');
     }
 };
