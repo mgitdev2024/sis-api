e00ffe4861bc7bbba1671766fecb8894d35d3d0d
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2024-06-18 13:26:46 +0800

    Warehouse Receive

--- app/Http/Controllers/v1/WMS/Warehouse/WarehouseReceivingController.php
+++ app/Http/Controllers/v1/WMS/Warehouse/WarehouseReceivingController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\v1\WMS\Warehouse;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\v1\QualityAssurance\SubStandardItemController;
 use App\Models\MOS\Production\ProductionBatchModel;
+use App\Models\QualityAssurance\SubStandardItemModel;
 use App\Models\Settings\WarehouseLocationModel;
 use App\Models\WMS\Settings\ItemMasterData\ItemMasterdataModel;
 use App\Models\WMS\Warehouse\WarehouseReceivingModel;
@@ -105,15 +107,13 @@ class WarehouseReceivingController extends Controller
     {
         return $this->readRecordById(WarehouseReceivingModel::class, $id, 'Warehouse Receiving');
     }
-    public function onUpdate(Request $request)
+    public function onUpdate(Request $request, $referenceNumber)
     {
         $fields = $request->validate([
             'scanned_items' => 'required|string', // {slid:1}
-            'reference_number' => 'required|string'
         ]);
         try {
             $scannedItems = null;
-            $referenceNumber = $fields['reference_number'];
             $dataEncodedItems = json_decode($fields['scanned_items'], true);
             if (isset($dataEncodedItems['slid'])) {
                 $scannedItems = $this->onGetQueuedItems($dataEncodedItems['slid'], false);
@@ -133,7 +133,6 @@ class WarehouseReceivingController extends Controller
     {
         try {
             DB::beginTransaction();
-            $receivedQuantity = 0;
             foreach ($layers as $layerValue) {
                 foreach ($layerValue as $itemDetails) {
                     $productionBatch = ProductionBatchModel::find($itemDetails['bid']);
@@ -147,7 +146,6 @@ class WarehouseReceivingController extends Controller
                         $decodeItems[$itemDetails['sticker_no']]['status'] = 3;
                         $productionItem->produced_items = json_encode($decodeItems);
                         $productionItem->save();
-                        $receivedQuantity++;
 
                         $warehouseReceiving = WarehouseReceivingModel::where('reference_number', $referenceNumber)
                             ->where('production_order_id', $productionBatch->production_order_id)
@@ -176,7 +174,6 @@ class WarehouseReceivingController extends Controller
     {
         try {
             DB::beginTransaction();
-            $receivedQuantity = 0;
             foreach ($scannedItems as $itemDetails) {
                 $productionBatch = ProductionBatchModel::find($itemDetails['bid']);
                 $productionItem = $productionBatch->productionItems;
@@ -189,7 +186,6 @@ class WarehouseReceivingController extends Controller
                     $decodeItems[$itemDetails['sticker_no']]['status'] = 3;
                     $productionItem->produced_items = json_encode($decodeItems);
                     $productionItem->save();
-                    $receivedQuantity++;
 
                     $warehouseReceiving = WarehouseReceivingModel::where('reference_number', $referenceNumber)
                         ->where('production_order_id', $productionBatch->production_order_id)
@@ -217,5 +213,65 @@ class WarehouseReceivingController extends Controller
             in_array($producedItems[$itemKey]['status'], $inclusionArray) :
             !in_array($producedItems[$itemKey]['status'], $exclusionArray);
         return $producedItems[$itemKey]['sticker_status'] != 0 && $inArrayFlag;
+    }
+
+    public function onSubStandard(Request $request, $referenceNumber)
+    {
+        $fields = $request->validate([
+            'created_by_id' => 'required',
+            'scanned_items' => 'required',
+            'reason' => 'required',
+            'attachment' => 'nullable',
+        ]);
+        try {
+            DB::beginTransaction();
+            $createdById = $fields['created_by_id'];
+            $scannedItems = json_decode($fields['scanned_items'], true);
+            $reason = $fields['reason'];
+            $attachment = $fields['attachment'] ?? null;
+            $locationId = 3; // Warehouse Receiving
+            foreach ($scannedItems as $itemDetails) {
+                $productionBatch = ProductionBatchModel::find($itemDetails['bid']);
+                $productionItem = $productionBatch->productionItems;
+                $productionOrderToMake = $productionBatch->productionOtb ?? $productionBatch->productionOta;
+                $itemCode = $productionOrderToMake->item_code;
+                $inclusionArray = [2];
+                $flag = $this->onItemCheckHoldInactiveDone(json_decode($productionItem->produced_items, true), $itemDetails['sticker_no'], $inclusionArray, []);
+                if ($flag) {
+                    $decodeItems = json_decode($productionItem->produced_items, true);
+                    $decodeItems[$itemDetails['sticker_no']]['status'] = 1.1;
+                    $productionItem->produced_items = json_encode($decodeItems);
+                    $productionItem->save();
+
+                    $warehouseReceiving = WarehouseReceivingModel::where('reference_number', $referenceNumber)
+                        ->where('production_order_id', $productionBatch->production_order_id)
+                        ->where('batch_number', $productionBatch->batch_number)
+                        ->where('item_code', $itemCode)
+                        ->first();
+                    if ($warehouseReceiving) {
+                        $warehouseReceiving->substandard_quantity = ++$warehouseReceiving->substandard_quantity;
+                        $warehouseReceiving->save();
+                    }
+                }
+            }
+
+            $substandardController = new SubStandardItemController();
+            $substandardRequest = new Request([
+                'created_by_id' => $createdById,
+                'scanned_items' => $fields['scanned_items'],
+                'reason' => $reason,
+                'attachment' => $attachment,
+                'location_id' => $locationId,
+            ]);
+
+            $substandardController->onCreate($substandardRequest);
+            DB::commit();
+            return $this->dataResponse('success', 201, 'Sub-Standard ' . __('msg.create_success'));
+
+        } catch (Exception $exception) {
+            DB::rollback();
+            dd($exception);
+            return $this->dataResponse('error', 400, 'Sub-Standard ' . __('msg.create_failed'));
+        }
     }
 }
