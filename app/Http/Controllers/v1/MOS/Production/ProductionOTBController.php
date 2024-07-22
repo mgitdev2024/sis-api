286d17f646012814967a2be036518c7bfd080307
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2024-07-22 09:11:03 +0800

    Update ProductionOTBController.php

--- app/Http/Controllers/v1/MOS/Production/ProductionOTBController.php
+++ app/Http/Controllers/v1/MOS/Production/ProductionOTBController.php
@@ -173,7 +173,8 @@ class ProductionOTBController extends Controller
 
                     $data = [
                         'produced_items' => json_encode([$itemDisposition->item_key => $producedItems[$itemDisposition->item_key]]),
-                        'production_batch_id' => $itemDisposition->production_batch_id
+                        'production_batch_id' => $itemDisposition->production_batch_id,
+                        'production_batch' => $itemDisposition->productionBatch
                     ];
                 } else {
                     $productionItem = $this->onSetProductionOrderBatch(
