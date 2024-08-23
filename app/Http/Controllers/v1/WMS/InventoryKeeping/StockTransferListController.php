e54bc80f8325acf61949eb1edd8e188cad0079df
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2024-08-23 13:51:12 +0800

    update in stock zone details

--- app/Http/Controllers/v1/WMS/InventoryKeeping/StockTransferListController.php
+++ app/Http/Controllers/v1/WMS/InventoryKeeping/StockTransferListController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\v1\WMS\InventoryKeeping;
 
 use App\Http\Controllers\Controller;
+use App\Models\WMS\InventoryKeeping\StockTransferItemModel;
 use App\Models\WMS\InventoryKeeping\StockTransferListModel;
 use Illuminate\Http\Request;
 use DB;
@@ -19,7 +20,18 @@ class StockTransferListController extends Controller
         try {
             DB::beginTransaction();
             $stockTransferListModel = new StockTransferListModel();
-            dd(json_decode($fields['items_to_transfer'], true));
+            $referenceCode = StockTransferListModel::onGenerateStockRequestReferenceNumber();
+            $latestId = StockTransferListModel::latest()->value('id') + 1;
+
+            foreach (json_decode($fields['items_to_transfer'], true) as $item) {
+                $stockTransferItemModel = new StockTransferItemModel();
+                $stockTransferItemModel->stock_transfer_list_id = $latestId;
+
+            }
+            $stockTransferListModel->reference_number = $referenceCode;
+            $stockTransferListModel->requested_item_count = 0;
+            $stockTransferListModel->reason = $fields['reason'];
+            $stockTransferListModel->save();
             DB::commit();
         } catch (Exception $exception) {
             DB::rollBack();
@@ -27,3 +39,60 @@ class StockTransferListController extends Controller
         }
     }
 }
+/*[
+                {
+                    "sub_location_id": 1,
+                    "code": "RCK0001",
+                    "layers":[
+                        {
+                            "item_code": "CR 12",
+                            "initial_stock": 10,
+                            "transfer_quantity": 5,
+                            "layer": 1
+                        },
+                        {
+                            "item_code": "CR 12",
+                            "initial_stock": 10,
+                            "transfer_quantity": 5,
+                            "layer": 2
+                        },
+                        {
+                            "item_code": "CR 12",
+                            "initial_stock": 10,
+                            "transfer_quantity": 5,
+                            "layer": 3
+                        }
+                    ]
+                },
+                {
+                    "sub_location_id": 2,
+                    "code": "RCK0002",
+                    "layers":[
+                        {
+                            "item_code": "CR 12",
+                            "initial_stock": 10,
+                            "transfer_quantity": 5,
+                            "layer": 1
+                        },
+                        {
+                            "item_code": "MM 6",
+                            "initial_stock": 10,
+                            "transfer_quantity": 5,
+                            "layer": 1
+                        },
+                        {
+                            "item_code": "CR 12",
+                            "initial_stock": 10,
+                            "transfer_quantity": 5,
+                            "layer": 2
+                        },
+                        {
+                            "item_code": "CR 12",
+                            "initial_stock": 10,
+                            "transfer_quantity": 5,
+                            "layer": 3
+                        }
+                    ]
+                }
+            ]
+ */
