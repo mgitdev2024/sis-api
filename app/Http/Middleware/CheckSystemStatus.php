726f5212d798a39896f20b0e471aee132d053fde
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2024-09-18 13:18:10 +0800

    Update CheckSystemStatus.php

--- app/Http/Middleware/CheckSystemStatus.php
+++ app/Http/Middleware/CheckSystemStatus.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Access\ScmSystemModel;
+use App\Models\Admin\System\ScmSystemModel;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
