0254d3f9df6feea3ded3e90222c60adbc8031211
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2024-10-11 14:57:19 +0800

    Update StockInventoryController.php

--- app/Http/Controllers/v1/WMS/Storage/StockInventoryController.php
+++ app/Http/Controllers/v1/WMS/Storage/StockInventoryController.php
@@ -107,18 +107,20 @@ class StockInventoryController extends Controller
             $stockInventories = ItemMasterdataModel::select(
                 'im.item_code as item_code',
                 'im.description as description',
+                'im.status as item_status',
                 'im.id as id',
                 'ic.name as category',
                 DB::raw('COALESCE(si.stock_count, 0) as stock_count'),
-                DB::raw('CASE
-                            WHEN si.status = 0 OR si.status IS NULL THEN "Inactive"
-                            WHEN si.status = 1 THEN "Active"
-                        END as stock_status')
+                // DB::raw('CASE
+                //             WHEN si.status = 0 OR si.status IS NULL THEN "Inactive"
+                //             WHEN si.status = 1 THEN "Active"
+                //         END as stock_status')
             )
                 ->from('wms_item_masterdata as im')
                 ->leftJoin('wms_item_categories as ic', 'im.item_category_id', '=', 'ic.id')
                 ->leftJoin('wms_stock_inventories as si', 'im.id', '=', 'si.item_id')
-                ->orderByRaw('CASE WHEN si.status IS NULL THEN 1 ELSE 0 END, si.status DESC')
+                // ->orderByRaw('CASE WHEN si.status IS NULL THEN 1 ELSE 0 END, si.status DESC')
+                ->orderBy('stock_count', 'DESC')
                 ->get();
             $stockInventories->each->setAppends([]);
             return $this->dataResponse('success', 200, 'Stock Inventories ' . __('msg.record_found'), $stockInventories);
