6e53cf1c679e4d7c7a196d89d5c03ceb40980e5c
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2025-05-22 10:41:52 +0800

    Update PurchaseOrderModel.php

--- app/Models/PurchaseOrder/PurchaseOrderModel.php
+++ app/Models/PurchaseOrder/PurchaseOrderModel.php
@@ -17,7 +17,9 @@ class PurchaseOrderModel extends Model
         'formatted_purchase_order_date_label',
         'formatted_expected_delivery_date_label',
         'formatted_created_at_label',
-        'formatted_updated_at_label'
+        'formatted_updated_at_label',
+        'created_by_name_label',
+        'updated_by_name_label',
     ];
     protected $fillable = [
         'reference_number',
@@ -71,5 +73,22 @@ class PurchaseOrderModel extends Model
         return $this->expected_delivery_date
             ? Carbon::parse($this->expected_delivery_date)->format('F d, Y')
             : null;
+    }
+
+    public function getCreatedByNameLabelAttribute()
+    {
+        $userModel = \App\Models\User::where('employee_id', $this->created_by_id)->first();
+        if ($userModel) {
+            return $userModel->first_name . ' ' . $userModel->last_name;
+        }
+        return null;
+    }
+    public function getUpdatedByNameLabelAttribute()
+    {
+        $userModel = \App\Models\User::where('employee_id', $this->updated_by_id)->first();
+        if ($userModel) {
+            return $userModel->first_name . ' ' . $userModel->last_name;
+        }
+        return null;
     }
 }
