26486b5fa2f572b220d14d2cc07358143376d7f6
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2025-10-10 10:11:14 +0800

    Add department item retrieval to inventory count
    Introduced onGetItemByDepartment method in StockInventoryCountController to fetch items by department. Updated API routes and config to support new endpoint and URL. Also refactored item code handling for inventory count creation and item diff logic.

--- config/apiurls.php
+++ config/apiurls.php
@@ -20,5 +20,6 @@ return [
         'stock_conversion_item_id_get' => '/stock/conversion/item-id/get/',
         'public_stock_count_lead_time_current_get' => '/public/stock-count-lead-time/current/get',
         'stock_conversion_item_id_get_auto_convert' => '/stock/conversion/item-id/get-auto-convert/',
+        'get_item_by_department' => '/item-by-department/get/'
     ],
 ];
