07ddfa9a7c0c01171f7155205d3426e8cc68e426
LoedeBenitez <57516183+LoedeBenitez@users.noreply.github.com>
2025-10-13 18:07:57 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:create-store-receiving-inventory')->everyMinute()->withoutOverlapping();
+        $schedule->command('app:create-store-receiving-inventory')->everyMinute();
         $schedule->command('app:delete-store-consolidation-cache')->dailyAt('00:00')->withoutOverlapping();
 
         // Add your queue worker
